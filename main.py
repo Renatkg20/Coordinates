f5382826a993ed7d3af00e6b5a5db1dc9db9ba26
Rinat <61133934+Renatkg20@users.noreply.github.com>
2022-11-04 22:32:30 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,40 @@
+n = int(input())
+
+l0 = []
+l1 = []
+total = []
+for i in range(n):
+    n1 = input()
+    t = n1.split(" ")
+    l0.append(int(t[0]))
+    l1.append(int(t[1]))
+
+
+for a, b in zip(l0, l1):
+    if a > 0 and b > 0:
+      
+        # print("Первая четверть")
+        total.append("Первая четверть")
+    elif a < 0 and b > 0: 
+        # print("Вторая четверть")
+        total.append("Вторая четверть")
+    elif a < 0 and b < 0: 
+        # print("Третья четверть")
+        total.append("Третья четверть")
+    elif a > 0 and b < 0: 
+        # print("Четвертая четверть")
+        total.append("Четвертая четверть")
+
+u = "".join(total)
+
+q1 = u.count("Первая четверть")
+q2 = u.count("Вторая четверть")
+q3 = u.count("Третья четверть")
+q4 = u.count("Четвертая четверть")
+
+print(f"Первая четверть: {q1}")
+print(f"Вторая четверть: {q2}")
+print(f"Третья четверть: {q3}")
+print(f"Четвертая четверть: {q4}")
+
+
